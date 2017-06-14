1a4a89bdbfdc9455ef0d962701e75b0d4e1b8da5
ericsu1998 <esu1@andrew.cmu.edu>
2017-06-13 22:37:02 -0400

    Reading and Visualizing Data

--- research task2.py
+++ research task2.py
@@ -48,6 +48,19 @@ def gatherData(env, episodes, policy, filename, colorMode = 'gray'):
 				i = 0
 				break	
 
+"""
+Add code for reading back the data from an specific episode and even visualize at random a small subset of the images. 
+Create a function for reading the data and another for testing and visualizing the data. 
+The test can be just to print on screen data size as you already have for the data storage part but from the loaded data. 
+Basically create a function that loads the data into memory and another to visualize specified number of random frames 
+and the size of the datasets. 
+"""
+def readData(filename, episode):
+
+
+######################
+##TESTS
+######################
 def testGatherData():
 	testEnv = gym.make('CartPole-v0')
 	episodes = 5
@@ -63,7 +76,14 @@ def testGatherData():
 			print("")
 		print("")
 
-testGatherData()
+def testReadData():
+	#Tests
 
+def testAll():
+	testGatherData()
+	testReadData()
+
+if (__name__ == "__main__"):
+	testAll()
 
 
---
+++ task3.py
@@ -0,0 +1,8 @@
+"""
+Add code for reading back the data from an specific episode and even visualize at random a small subset of the images. 
+Create a function for reading the data and another for testing and visualizing the data. 
+The test can be just to print on screen data size as you already have for the data storage part but from the loaded data. 
+Basically create a function that loads the data into memory and another to visualize specified number of random frames 
+and the size of the datasets. 
+"""
+def readData(filename, episode):
